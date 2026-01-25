48495517d429add762e7d05aa3e61a7b471ba77d
lisakeyy <lisakeyy@gmail.com>
2026-01-25 20:27:36 +0300

    Return Rectangle desc to traditional (not record)

--- sandbox/src/main/java/by/lisakeyy/geometry/figures/Square.java
+++ sandbox/src/main/java/by/lisakeyy/geometry/figures/Square.java
@@ -1,12 +1,6 @@
 package by.lisakeyy.geometry.figures;
 
-public class Square {
-
-    private double side;
-
-    public Square(double side) {
-        this.side = side;
-    }
+public record Square(double side) {
 
     public static void printSquareArea(Square s) {
         String text = String.format("Square's area with a side %f = %f", s.side, s.area());
