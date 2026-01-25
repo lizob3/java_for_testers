15c6faf280f0a150b564eae5e2e5870f3946ad05
lisakeyy <lisakeyy@gmail.com>
2026-01-25 18:03:39 +0300

    Experiments with functions

--- sandbox/src/main/java/Hello.java
+++ sandbox/src/main/java/Hello.java
@@ -1,5 +1,15 @@
 public class Hello {
-    static void main() {
+    public static void main() {
         System.out.println("Hello world!");
+        System.out.println(2 + 2);
+        System.out.println(2 * 2);
+        System.out.println(2 - 2);
+        System.out.println(2 / 2);
+        System.out.println((2 + 2) * 2);
+        System.out.println("Hello " + "world");
+
+        System.out.println("2 + 2 = " + 2 + 2);
+        System.out.println("2 + 2 = " + (2 + 2));
+        System.out.println(2 + 2 + " = 4");
     }
 }
