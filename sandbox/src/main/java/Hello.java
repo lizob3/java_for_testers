788604814dcaa6f5e90f0a81114d52f9bd95eb46
lisakeyy <lisakeyy@gmail.com>
2026-01-31 16:11:40 +0300

    Experiments with exeptions

--- sandbox/src/main/java/Hello.java
+++ sandbox/src/main/java/Hello.java
@@ -2,8 +2,15 @@ import java.io.File;
 
 public class Hello {
     public static void main() {
-        System.out.println("Hello world!");
-        System.out.println(2 + 2);
+        try {
+            var z = calculate();
+            System.out.println(z);
+        } catch (ArithmeticException exeption) {
+            exeption.printStackTrace();
+        }
+
+
+        /*System.out.println(2 + 2);
         System.out.println(2 * 2);
         System.out.println(2 - 2);
         System.out.println(2 / 2);
@@ -17,6 +24,18 @@ public class Hello {
 
         var configFile = new File("sandbox/build.gradle.kts");
         System.out.println(configFile.getAbsoluteFile());
-        System.out.println(configFile.exists());
+        System.out.println(configFile.exists());*/
+    }
+
+    private static int calculate() {
+        var x = 1;
+        var y = 1;
+        var z = devide(x, y);
+        return z;
+    }
+
+    private static int devide(int x, int y) {
+        var z = x / y;
+        return z;
     }
 }
