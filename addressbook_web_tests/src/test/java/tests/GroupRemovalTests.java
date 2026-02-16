ecf0b566a158b58c7b2db47a9355023e4e262bc5
lisakeyy <lisakeyy@gmail.com>
2026-02-16 22:53:01 +0300

    Create cycles in tests for removing and creating groups

--- addressbook_web_tests/src/test/java/tests/GroupRemovalTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupRemovalTests.java
@@ -17,4 +17,13 @@ public class GroupRemovalTests extends TestBase {
         Assertions.assertEquals(groupCount - 1, newGroupCount);
     }
 
+    @Test
+    void canRemoveAllGroupsAtOnce() {
+        if (app.groups().getCount() == 0) {
+            app.groups().createGroup(new GroupData("name name", "name header", "name footer"));
+        }
+        app.groups().removeAllGroups();
+        Assertions.assertEquals(0, app.groups().getCount());
+    }
+
 }
