2a3ea53d7b03982155272315d5afdc920d54c031
lisakeyy <lisakeyy@gmail.com>
2026-03-01 22:04:01 +0300

    Update test for Groups Creation with info from Database

--- addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -21,6 +21,8 @@ public class ApplicationManager {
 
     private Properties properties;
 
+    private JdbcHelper jdbc;
+
     public void init(String browser, Properties properties) {
         this.properties = properties;
         if (driver == null) {
@@ -56,6 +58,13 @@ public class ApplicationManager {
             contacts = new ContactHelper(this);
         }
         return contacts;
+    }
+
+    public JdbcHelper jdbc() {
+        if (jdbc == null) {
+            jdbc = new JdbcHelper(this);
+        }
+        return jdbc;
     }
 
     public boolean isElementPresent(By locator) {
--- addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
@@ -1,21 +1,15 @@
 package tests;
 
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import common.CommonFunctions;
 import model.GroupData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -54,23 +48,40 @@ public class GroupCreationTests extends TestBase {
         return result;
     }
 
+    public static List<GroupData> singleRandomGroupProvider()  {
+        return List.of(new GroupData()
+                .withName(CommonFunctions.randomString(10))
+                .withHeader(CommonFunctions.randomString(15))
+                .withFooter(CommonFunctions.randomString(20)));
+    }
+
 
     @ParameterizedTest
-    @MethodSource("groupProvider")
-    public void canCreateMultipleGroup(GroupData group) {
-        var oldGroups = app.groups().getList();
+    @MethodSource("singleRandomGroupProvider")
+    public void canCreateGroup(GroupData group) {
+        var oldGroups = app.jdbc().getGroupList();
         app.groups().createGroup(group);
-        var newGroups = app.groups().getList();
+        var newGroups = app.jdbc().getGroupList();
         Comparator<GroupData> compareById = (o1, o2) -> {
             return Integer.compare(Integer.parseInt(o1.id()), Integer.parseInt(o2.id()));
         };
         newGroups.sort(compareById);
+        var maxId = newGroups.get(newGroups.size() - 1).id();
 
         var expectedList = new ArrayList<>(oldGroups);
-        expectedList.add(group.withId(newGroups.get(newGroups.size() - 1).id()).withHeader("").withFooter(""));
+        expectedList.add(group.withId(maxId));
         expectedList.sort(compareById);
 
         Assertions.assertEquals(newGroups, expectedList);
+
+        var newUiGroups = app.groups().getList();
+        newUiGroups.sort(compareById);
+        var newGroupsWithEmptyFields = new ArrayList<GroupData>();
+        for (var newGroup : newGroups) {
+            newGroupsWithEmptyFields.add(newGroup.withHeader("").withFooter(""));
+        }
+        Assertions.assertEquals(newGroupsWithEmptyFields, newUiGroups);
+
     }
 
     @ParameterizedTest
