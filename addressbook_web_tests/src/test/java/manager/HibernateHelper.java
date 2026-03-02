3b3a33c34c95f89b1df80b8633c78e0c7cac4edc
lisakeyy <lisakeyy@gmail.com>
2026-03-02 22:21:28 +0300

    Use ORM (with Hibernate) to create groups in DB

--- addressbook_web_tests/src/test/java/manager/HibernateHelper.java
+++ addressbook_web_tests/src/test/java/manager/HibernateHelper.java
@@ -16,7 +16,6 @@ public class HibernateHelper extends HelperBase {
     public HibernateHelper(ApplicationManager manager) {
         super(manager);
         sessionFactory = new Configuration()
-            //.addAnnotatedClass(Book.class)
             .addAnnotatedClass(GroupRecord.class)
             .setProperty(AvailableSettings.URL, "jdbc:mysql://localhost/addressbook")
             // Credentials
@@ -37,9 +36,31 @@ public class HibernateHelper extends HelperBase {
         return new GroupData("" + record.id, record.name, record.header, record.footer);
     }
 
+    private static GroupRecord convert(GroupData data) {
+        var id = data.id();
+        if ("".equals(id)) {
+            id = "0";
+        }
+        return new GroupRecord(Integer.parseInt(id), data.name(), data.header(), data.footer());
+    }
+
     public List<GroupData> getGroupList() {
         return convertList(sessionFactory.fromSession(session -> {
             return session.createQuery("from GroupRecord", GroupRecord.class).list();
         }));
+    }
+
+    public long getGroupCount() {
+        return sessionFactory.fromSession(session -> {
+            return session.createQuery("select count (*) from GroupRecord", Long.class).getSingleResult();
+        });
+    }
+
+    public void createGroup(GroupData data) {
+        sessionFactory.inSession(session -> {
+            session.getTransaction().begin();
+            session.persist(convert(data));
+            session.getTransaction().commit();
+        });
     }
 }
