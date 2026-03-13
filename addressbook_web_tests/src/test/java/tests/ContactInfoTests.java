5294791f8e4e26799a49dbc795ff16fcfdad8833
lisakeyy <lisakeyy@gmail.com>
2026-03-13 23:32:47 +0300

    Update test for checking Contact info using Map

--- addressbook_web_tests/src/test/java/tests/ContactInfoTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactInfoTests.java
@@ -15,12 +15,19 @@ public class ContactInfoTests extends TestBase {
             app.hbm().createContact(
                     new ContactData("", "Name", "Lastname", "Address", "test@test.com", "+375291112233", "", "", ""));
         }
+
         var contacts = app.hbm().getContactList();
-        var contact = contacts.get(0);
-        var phones = app.contacts().getPhones(contact);
-        var expected = Stream.of(contact.homePhone(), contact.mobilePhone(), contact.workPhone())
-                .filter(s -> s != null && ! "".equals(s))
-                .collect(Collectors.joining("\n"));
+        var expected = contacts.stream().collect(Collectors.toMap(contact -> contact.id(), contact ->
+            Stream.of(contact.homePhone(), contact.mobilePhone(), contact.workPhone())
+                    .filter(s -> s != null && !"".equals(s))
+                    .collect(Collectors.joining("\n"))
+        ));
+        var phones = app.contacts().getPhones();
         Assertions.assertEquals(expected, phones);
+    }
+
+    @Test
+    void testInfo() {
+        
     }
 }
