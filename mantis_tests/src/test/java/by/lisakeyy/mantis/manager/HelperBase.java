86900e387f16a94ffe51e000d585575ca4f85093
lisakeyy <lisakeyy@gmail.com>
2026-03-17 22:59:38 +0300

    Prepare new project for Mantis

---
+++ mantis_tests/src/test/java/by/lisakeyy/mantis/manager/HelperBase.java
@@ -0,0 +1,39 @@
+package by.lisakeyy.mantis.manager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.nio.file.Paths;
+
+public class HelperBase {
+
+    protected final ApplicationManager manager;
+
+    public HelperBase(ApplicationManager manager) {
+        this.manager = manager;
+    }
+
+    protected void click(By locator) {
+        manager.driver().findElement(locator).click();
+    }
+
+    protected void type(By locator, String text) {
+        click(locator);
+        manager.driver().findElement(locator).clear();
+        manager.driver().findElement(locator).sendKeys(text);
+    }
+
+    protected void attach(By locator, String file) {
+        manager.driver().findElement(locator).sendKeys(Paths.get(file).toAbsolutePath().toString());
+    }
+
+    protected void selectAllCheckboxes() {
+        manager.driver()
+                .findElements(By.name("selected[]"))
+                .forEach(WebElement::click);
+    }
+
+    protected boolean isElementPresent(By locator) {
+        return manager.driver().findElements(locator).size() > 0;
+    }
+}
